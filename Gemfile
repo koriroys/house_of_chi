887a309ee5102a3912f5c8442992b3c519d8ba7f
Kori Roys <kori@koriroys.com>
2012-06-03 16:00:03 -0500

    ready to rock

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+# Platform
+gem 'rails', '3.2.3'
+gem 'sqlite3'
+gem 'jquery-rails'
+
+# Application
+gem 'koala'
+gem 'haml-rails'
+
+group :development, :test do
+  gem 'factory_girl_rails'
+  gem 'rspec-rails'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HouseOfChiPlaylist::Application.config.secret_token = 'dc1a2e9e3b4eae80549550ed4217c40f53e03b079e9a3a184e3e5d40b7c78e287a1ac49b53fd7536c60e3fc97e326ed8dac5e122f9d553f29634f239e791344e'
