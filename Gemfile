19b0471605f16e2612ca00eca63b145e0137a8c8
Kori Roys <kori@koriroys.com>
2012-06-05 16:41:51 -0500

    facebook omniauth is getting stuffed in so I don't have to get a new access_key every 20 minutes

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.3'
 gem 'sqlite3'
 gem 'jquery-rails'
+gem 'omniauth-facebook'
 
 # Application
 gem 'koala'
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ HouseOfChiPlaylist::Application.routes.draw do
   root :to => 'playlists#index'
 
   get '/tracks' => 'tracks#index'
+
+  get '/auth/facebook/callback' => 'sessions#create'
 end
