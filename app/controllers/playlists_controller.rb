44ce395bfd9b096d26760acae471a7818f2ed50c
Kori Roys <kori@koriroys.com>
2012-06-06 10:51:09 -0500

    regex comment for stripping out youtube video ids

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -4,6 +4,8 @@ class PlaylistsController < ApplicationController
     graph = Koala::Facebook::API.new(session[:fb_token])
     @array = graph.get_connections('179298008840024', 'feed', {fields: 'id,from,link,created_time'})
     @hoc = @array.map{ |item| item if item['link'] =~ /youtube|soundcloud/i }.compact
+    # just peel out the youtube video id
+    # link.slice(/(v=)(\w*)/, 2)
   end
 
 end
