243900f9cfb7b5b495c47c2770670b16cbd991bd
Kori Roys <kori@koriroys.com>
2012-08-21 11:13:14 -0500

    moving tests around

---
+++ spec/controllers/playlists_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe PlaylistsController do
+  describe '#index' do
+    it 'keeps you from getting into trouble' do
+      #expect { get :index }.to raise_error(Koala::Facebook::APIError)
+    end
+  end
+end
