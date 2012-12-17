d3c6e889a048a5c4070f8952b66d2b38e5d5efc0
Kori Roys <kori@koriroys.com>
2012-12-17 00:01:49 -0600

    better parser for posts

--- lib/post_parser.rb
+++ lib/post_parser.rb
@@ -1,9 +1,31 @@
 class PostParser
   def initialize(options)
-
+    self.raw_posts = options[:raw_posts][:data]
   end
 
   def process
-    [2,1]
+    tracks = raw_posts.each.with_object([]) do |post, tracks|
+      tracks << PostTrackInfo.new(post: post)
+    end
+    tracks.select{ |t| matches(t.link) }
   end
+
+  def matches(link)
+    link.match(/https?:\/\/w*\.?(youtu\.be|youtube|soundcloud)(\.com)?/)
+  end
+
+  private
+  attr_accessor :raw_posts
+end
+
+class PostTrackInfo
+  attr_reader :link
+  def initialize(options)
+    self.post = options[:post]
+    self.link = post[:link] || ''
+  end
+
+  private
+  attr_writer :link
+  attr_accessor :post
 end
--- spec/lib/post_parser_spec.rb
+++ spec/lib/post_parser_spec.rb
@@ -4,9 +4,11 @@ require 'json'
 
 describe PostParser do
   it 'takes a json list of posts and returns a list of tracks to create' do
-    posts = JSON.parse(File.read(File.join(directory, %w(.. fixtures posts.json))))
-    p = PostParser.new(posts: posts)
+    raw_posts = JSON.parse(File.read(File.join(directory, %w(.. fixtures posts.json))), symbolize_names: true)
+    p = PostParser.new(raw_posts: raw_posts)
     tracks = p.process
-    expect(tracks.count).to eq(2)
+    puts tracks.map(&:link)
+    expect(tracks.count).to eq(31)
+    expect(tracks.first.link).to eq('https://soundcloud.com/ssma/solomun-mixmag-stream-2012-12')
   end
 end
